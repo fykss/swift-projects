7c49dbab2f94ff2bcd6186e8521f183678a95669
Влад Летун <fyks1649@gmail.com>
2020-03-26 18:42:45 +0200

    Clima Project in process

---
+++ Clima-iOS13/Clima/WeatherManagerModel.swift
@@ -0,0 +1,62 @@
+//
+//  WeatherManagerModel.swift
+//  Clima
+//
+//  Created by Vladyslav Lietun on 24.03.2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManagerModel, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+
+struct WeatherManagerModel {
+    let key = "d1d9930193c9fdf1d7ca20b88bfcdca3"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?"
+    
+    var delegate: WeatherManagerDelegate?
+    
+    func fetchWeather(cityName: String) {
+        performRequest(with: "\(weatherURL)q=\(cityName)&appid=\(key)")
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    if let weather = self.parseJSON(safeData) {
+                        self.delegate?.didUpdateWeather(self, weather: weather)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ weatherData: Data) -> WeatherModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodeData = try decoder.decode(WeatherData.self, from: weatherData)
+            
+            let id = decodeData.weather[0].id
+            let temp = decodeData.main.temp
+            let name = decodeData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            return weather
+            
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+    
+}
