bb6bd40c177aab381d43d23edbc93ef14e092b6c
Влад Летун <fyks1649@gmail.com>
2020-03-28 17:50:58 +0200

    Firebase in Flash Chat

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,11 +14,12 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
-        let titleText = titleLabel.text!
+        let titleText = K.appName
         titleLabel.text = ""
         var charIndex = 0.0
-        
+
         for letter in titleText {
             Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
                 self.titleLabel.text?.append(letter)
